cb31a8670b85140869216052efd9058fd3dd555c
Cindy91 <cindy.sp91@gmail.com>
2022-11-11 16:25:46 -0300

    Actualizado pedidosYaDB.sql

--- pedidosYaDB.sql
+++ pedidosYaDB.sql
@@ -31,6 +31,8 @@ CREATE TABLE pedido (
 	FOREIGN KEY(usuario) references usuario(id_usuario)
 );
 
+--Insert data.
+
 INSERT INTO usuario values (001, 'Pedro Alonso', 'p123', 'calle falsa 123');
 INSERT INTO usuario values (002, 'Maria Perez', 'mary666', 'diagonal 60 334');
 INSERT INTO usuario values (003, 'Raúl Gómez', 'raulito22', 'calle 560 1256');
@@ -44,3 +46,41 @@ INSERT INTO producto values (102, 'Ensalada cesar', 'Vida light');
 INSERT INTO producto values (103, 'Pollo al spiedo con guarnición', 'El imperio del pollo');
 INSERT INTO producto values (104, 'Combo 15 piezas', 'Dos dragones');
 INSERT INTO producto values (105, 'Sixpack Andes Ipa', 'El baron de la birra');
+
+INSERT INTO factura values (24563, 3499.99, 'efectivo');
+INSERT INTO factura values (25699, 1899, 'débito');
+INSERT INTO factura values (26485, 960.5, 'efectivo');
+INSERT INTO factura values (27853, 2488.9, 'crédito');
+INSERT INTO factura values (28345, 3460, 'débito');
+
+INSERT INTO pedido values (340, 24563, 100, 001, 1, 3499.99);
+INSERT INTO pedido values (341, 25699, 101, 002, 1, 1899);
+INSERT INTO pedido values (342, 26485, 102, 003, 1, 960.5);
+INSERT INTO pedido values (343, 27853, 103, 004, 1, 2488.9);
+INSERT INTO pedido values (344, 28345, 104, 005, 1, 3460);
+
+--Consultas.
+
+/*a. Hacer un select que una los pedidos y los usuarios que los hicieron. Visualizar solo las columnas de
+nombre de usuario, el producto, la cantidad y el monto.*/
+
+
+/*b. Hacer un select que una los pedidos y las facturas de esos pedidos. Visualizar solo las columnas de
+producto, el monto y el medio de pago.*/
+
+
+/*c. Hacer un select que una los pedidos y los productos de esos pedidos. Visualizar solo las columnas
+de descripción, cantidad y monto.*/
+
+
+/*d. Crear una selección de todos los productos que son pagados con efectivo.*/
+
+
+/*e. Seleccionar los usuarios que hayan hecho algún pedido.*/
+
+
+/*f. Seleccionar todas las facturas por un monto mayor a 500.*/
+
+
+/*g. Cambiar todas las facturas que digan aplicación a débito.*/
+
